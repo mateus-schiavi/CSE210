e9b4abc8c10bfc11dc73e189a64ef97e16b7a368
Mateus Schiavi <mtsschiavi@gmail.com>
2023-02-02 17:02:28 -0300

    y

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
-class Program
+namespace PhysicsCalculations
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        static void Main(string[] args)
+        {
+            double initialVelocity = 10.0; // m/s
+            double acceleration = 9.8; // m/s^2
+            double time = 5.0; // s
+
+            double finalVelocity = initialVelocity + acceleration * time;
+
+            Console.WriteLine("The final velocity is: " + finalVelocity + " m/s");
+        }
     }
 }
