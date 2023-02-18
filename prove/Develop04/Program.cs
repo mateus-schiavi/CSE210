38d519013f7e0762a1252ffe9d3f5e377bf9ae14
Mateus Schiavi <mtsschiavi@gmail.com>
2023-02-18 15:21:21 -0300

    finished

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,55 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int choice = 0;
+
+        while (choice != 4)
+        {
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1. Breathing Exercise");
+            Console.WriteLine("2. Listing Exercise");
+            Console.WriteLine("3. Reflection Exercise");
+            Console.WriteLine("4. Quit");
+
+            try
+            {
+                choice = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+                continue;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Breath breath = new Breath();
+                    breath.DoBreathing();
+                    break;
+
+                case 2:
+                    Listing listing = new Listing();
+                    listing.DoListing();
+                    break;
+
+                case 3:
+                    Reflection reflection = new Reflection();
+                    reflection.DoReflection();
+                    break;
+
+                case 4:
+                    Console.WriteLine("Goodbye!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
     }
 }
