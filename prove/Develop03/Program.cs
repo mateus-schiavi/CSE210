4487d656169e48b0758ff9f6ad150d5687c356b0
Mateus Schiavi <mtsschiavi@gmail.com>
2023-02-19 14:55:31 -0300

    fixed

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -10,6 +10,7 @@ class Program
             // Create an array of Scripture objects
             Scripture[] scriptures = new Scripture[]
             {
+                new Scripture("Isaiah 8:13", "Sanctify the Lord of hosts himself; and let him be your fear, and let him be your dread", new string[] {"Sanctify", "the", "Lord", "of", "hosts", "himself;", "and", "let", "him", "be", "your", "fear,", "and", "let", "him", "be", "your", "dread."}),               
                 new Scripture("Isaiah 8:13", "Sanctify the Lord of hosts himself; and let him be your fear, and let him be your dread", new string[] {"Sanctify", "the", "Lord", "of", "hosts", "himself;", "and", "let", "him", "be", "your", "fear,", "and", "let", "him", "be", "your", "dread."}),
                 new Scripture("1 Peter 5:7", "Casting all your care upon him; for he careth for you.", new string[] {"Casting", "all", "your", "care", "upon", "him;", "for", "he", "careth", "for", "you."}),
                 new Scripture("John 3:16", "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.", new string[] {"For", "God", "so", "loved", "the", "world,", "that", "he", "gave", "his", "only", "begotten", "Son,", "that", "whosoever", "believeth", "in", "him", "should", "not", "perish,", "but", "have", "everlasting", "life."}),
@@ -18,9 +19,7 @@ class Program
                 new Scripture("James 1:4","But let patience have her perfect work, that ye may be perfect and entire, wanting nothing", new string[] {"But", "let","patience","have","her","perfect","work,","that","ye","may","be","perfect","and","entire", "wanting","nothing"}),
                 new Scripture("Revelations 22:21", "The grace of our Lord Jesus Christ be with you all, amen.", new string[] {"The", "grace", "of", "our", "Lord", "Jesus", "Christ", "be", "with","you", "all", "amen."}),
                 new Scripture("Isaiah 1:19","If ye be willing and obedient, ye shall eat the good of the land", new string[]{"If","ye","be","willing","and","obedient","ye","shall","eat","the","good","of","the","land"})
-                
-            };
-
+            };           
             Console.WriteLine("Welcome to the Scripture Memorizer Program!");
             Console.WriteLine("Below is a list of some scriptures:");
             Console.WriteLine("►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄►◄");
@@ -40,30 +39,15 @@ class Program
             Memorizing objMemorize = new Memorizing();
             objMemorize.Begin(selectedScripture);
 
+            // Ask the user if they would like to memorize another scripture
+            Console.WriteLine("Would you like to memorize another scripture? (y/n)");
+            string repeat = Console.ReadLine();
+
+            string fileName = "";
+            bool isFirst = true;
             while (true)
             {
-                Console.WriteLine("Would you like to memorize another scripture? (y/n)");
-                string repeat = Console.ReadLine();
-
-                if (repeat != "y")
-                {
-                    Console.WriteLine("Would you like to save this scripture to a file? (y/n)");
-                    string saveToFile = Console.ReadLine();
-
-                    if (saveToFile == "y")
-                    {
-                        Console.WriteLine("Please enter the name of the file to save the scripture to: ");
-                        string fileName = Console.ReadLine();
-                        string filePath = Path.Combine(Environment.CurrentDirectory, fileName);
-                        File.WriteAllText(filePath, selectedScripture.GetQuotation());
-                        Console.WriteLine($"The scripture has been saved to {filePath}.");
-                    }
-
-                    Console.WriteLine("Goodbye!");
-                    break;
-                }
-
-                Console.WriteLine("Please choose a scripture to memorize (Enter the number): ");
+                Console.WriteLine("Please choose a scripture to memorize (Enter the number), or type 'quit' to exit: ");
 
                 string input = Console.ReadLine();
                 if (input == "quit")
@@ -76,7 +60,33 @@ class Program
                 Scripture selectedNumber = scriptures[number - 1];
 
                 objMemorize.Begin(selectedNumber);
-                selectedScripture = selectedNumber;
+
+                Console.WriteLine("Would you like to save this scripture to a file? (y/n)");
+                string saveToFile = Console.ReadLine();
+                if (saveToFile == "y")
+                {
+                    if (isFirst)
+                    {
+                        Console.WriteLine("Please enter the name of the file to save the scriptures to: ");
+                        fileName = Console.ReadLine();
+                        isFirst = false;
+                    }
+
+                    string filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+                    using (StreamWriter sw = File.AppendText(filePath))
+                    {
+                        sw.WriteLine(selectedNumber.GetQuotation());
+                    }
+                    Console.WriteLine($"The scripture has been saved to {filePath}.");
+                }
+
+                Console.WriteLine("Would you like to memorize another scripture? (y/n)");
+                string again = Console.ReadLine();
+                if (again != "y")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
             }
         }
         catch (Exception ex)
@@ -85,3 +95,4 @@ class Program
         }
     }
 }
+   
