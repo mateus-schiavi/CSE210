d5e63b1f01239816bc40b6cea4e9ffde5ba1b6bf
Mateus Schiavi <mtsschiavi@gmail.com>
2023-02-20 01:19:42 -0300

    fixed

--- prove/Develop03/Memorizing.cs
+++ prove/Develop03/Memorizing.cs
@@ -30,7 +30,7 @@ class Memorizing
         for (int i = 0; i < verseText.Length; i++)
         {
             Console.Write(verseText[i]);
-            Thread.Sleep(50);
+            Thread.Sleep(1000);
         }
     }
 }
