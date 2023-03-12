8fb9f53558690ddc9b41b0e3303160ed05256c85
Mateus Schiavi <mtsschiavi@gmail.com>
2023-03-12 13:23:13 -0300

    fixed brackets

--- prove/Develop05/GoalTracker.cs
+++ prove/Develop05/GoalTracker.cs
@@ -4,41 +4,41 @@ namespace GoalTracker
 
 {
     public class Goal
-{
-    private string _fileName;
-    private List<string> _goals = new List<string>();
-    public Goal(){}
-
-    public virtual string Display()
     {
-        return "";
-    }
+        private string _fileName;
+        private List<string> _goals = new List<string>();
+        public Goal() { }
 
-    public void WriteTextFile(List<string> goals, string fileName)
-    {
-        _fileName = fileName;
-        _goals = goals;
-        using (StreamWriter outputFile = new StreamWriter(_fileName))
+        public virtual string Display()
         {
-            for(int i = 1; i < _goals.Count; i++)
+            return "";
+        }
+
+        public void WriteTextFile(List<string> goals, string fileName)
+        {
+            _fileName = fileName;
+            _goals = goals;
+            using (StreamWriter outputFile = new StreamWriter(_fileName))
             {
-                outputFile.WriteLine($"{i}. {_goals[i]}");
+                for (int i = 1; i < _goals.Count; i++)
+                {
+                    outputFile.WriteLine($"{i}. {_goals[i]}");
+                }
+            }
+        }
+
+        public void ReadTextFile(List<string> goals, string fileName)
+        {
+            _fileName = fileName;
+            _goals = goals;
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(",");
+                string items = parts[0];
+                goals.Add(items);
             }
         }
     }
-
-    public void ReadTextFile(List<string> goals, string fileName)
-    {
-        _fileName = fileName;
-        _goals = goals;
-        string[] lines = System.IO.File.ReadAllLines(fileName);
-
-        foreach (string line in lines)
-        {
-            string[] parts = line.Split(",");
-            string items = parts[0];
-            goals.Add(items);
-        }
-    }
-}
 }
