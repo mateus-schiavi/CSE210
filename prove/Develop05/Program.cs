03959e038e8cdd08c8f12e5b5bf5b9114f737ddd
Mateus Schiavi <mtsschiavi@gmail.com>
2023-03-12 14:44:12 -0300

    added options

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -3,59 +3,63 @@ using System;
 namespace GoalTracker
 {
     class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Goal Tracker app!");
-
-        bool exit = false;
-        while (!exit)
+        static void Main(string[] args)
         {
-            Console.WriteLine();
-            Console.WriteLine("Please select an option:");
-            Console.WriteLine("1. Add a personal goal");
-            Console.WriteLine("2. Delete a personal goal");
-            Console.WriteLine("3. View all personal goals");
-            Console.WriteLine("4. List personal goals");
-            Console.WriteLine("5. Record an event for a personal goal");
-            Console.WriteLine("6. Exit");
+            Console.WriteLine("Welcome to the Goal Tracker app!");
 
-            string input = Console.ReadLine();
-            switch (input)
+            bool exit = false;
+            while (!exit)
             {
-                case "1":
-                    PersonalGoal newGoal = new PersonalGoal();
-                    newGoal.AddGoal();
-                    newGoal.SaveToFile();
-                    break;
-                case "2":
-                    PersonalGoal goalToDelete = new PersonalGoal();
-                    goalToDelete.DeleteGoal();
-                    goalToDelete.SaveToFile();
-                    break;
-                case "3":
-                    PersonalGoal viewGoals = new PersonalGoal();
-                    viewGoals.ViewAllGoals();
-                    break;
-                case "4":
-                    PersonalGoal listGoals = new PersonalGoal();
-                    listGoals.ListGoals();
-                    break;
-                case "5":
-                    PersonalGoal recordEvent = new PersonalGoal();
-                    recordEvent.RecordEvent();
-                    recordEvent.SaveToFile();
-                    break;
-                case "6":
-                    PersonalGoal Quit = new PersonalGoal();
-                    Quit.Quit();
-                    break;
-                default:
-                    Console.WriteLine("Invalid option selected.");
-                    break;
+                Console.WriteLine();
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1. Add a personal goal");
+                Console.WriteLine("2. Delete a personal goal");
+                Console.WriteLine("3. View all personal goals");
+                Console.WriteLine("4. Save to a File");
+                Console.WriteLine("5. Load from File ");
+                Console.WriteLine("6. Record an event for a personal goal");
+                Console.WriteLine("7. Exit");
+
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        PersonalGoal newGoal = new PersonalGoal();
+                        newGoal.AddGoal();
+                        newGoal.SaveToFile();
+                        break;
+                    case "2":
+                        PersonalGoal goalToDelete = new PersonalGoal();
+                        goalToDelete.DeleteGoal();
+                        goalToDelete.SaveToFile();
+                        break;
+                    case "3":
+                        PersonalGoal viewGoals = new PersonalGoal();
+                        viewGoals.ViewAllGoals();
+                        break;
+                    case "4":
+                        PersonalGoal SaveGoals = new PersonalGoal();
+                        SaveGoals.SaveToFile();
+                        break;
+                    case "5":
+                        PersonalGoal loadFile = new PersonalGoal();
+                        loadFile.LoadFromFile();
+                        break;
+                    case "6":
+                        PersonalGoal recordEvent = new PersonalGoal();
+                        recordEvent.RecordEvent();
+                        break;
+                    case "7":
+                        PersonalGoal quit = new PersonalGoal();
+                        quit.Quit();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option selected.");
+                        break;
+                }
             }
         }
     }
-}
 
 }
