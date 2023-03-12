3bf8659a6eb1da9e8d63f15275e4b6b763444447
Mateus Schiavi <mtsschiavi@gmail.com>
2023-03-12 15:21:31 -0300

    Fixed Load And Save

--- prove/Develop05/PersonalGoal.cs
+++ prove/Develop05/PersonalGoal.cs
@@ -125,7 +125,6 @@ namespace GoalTracker
             Console.WriteLine("Goals saved to file successfully!");
         }
 
-
         public override void LoadFromFile()
         {
             verifier.VerifyFileIntegrity();
