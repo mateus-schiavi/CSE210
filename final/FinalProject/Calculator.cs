8ea82c84e0c3b1575dfdfb2a47d63e70f5c2e1a6
Mateus Schiavi <mtsschiavi@gmail.com>
2023-03-17 15:45:09 -0300

    added getters and setters

--- final/FinalProject/Calculator.cs
+++ final/FinalProject/Calculator.cs
@@ -1,6 +1,20 @@
 // Class 3: Calculator - abstract base class for all calculators
 public abstract class Calculator
 {
+    private double value1;
+    private double value2;
+
+    public double Value1
+    {
+        get { return value1; }
+        set { value1 = value; }
+    }
+
+    public double Value2
+    {
+        get { return value2; }
+        set { value2 = value; }
+    }
     // Abstract method to calculate a property
     public abstract double Calculate(double value1, double value2);
 }
