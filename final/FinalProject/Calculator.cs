05930e5b7cc168729b84f181ffe663bbc50b2fb3
Mateus Schiavi <mtsschiavi@gmail.com>
2023-03-17 00:14:26 -0300

    added one more parameter

--- final/FinalProject/Calculator.cs
+++ final/FinalProject/Calculator.cs
@@ -2,5 +2,5 @@
 public abstract class Calculator
 {
     // Abstract method to calculate a property
-    public abstract double Calculate(double value1, double value2);
+    public abstract double Calculate(double value1, double value2, double value3);
 }
